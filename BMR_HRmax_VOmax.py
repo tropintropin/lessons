238193fba1e4327e5d3d1c806fb7e55872403061
Valery Tropin <tropin.tropin@gmail.com>
2022-07-14 01:53:00 +0300

    Fix BMR_HRmax_VOmax.py
    Change numeric types in variables (from float to int).
    Change output formatting.

--- BMR_HRmax_VOmax.py
+++ BMR_HRmax_VOmax.py
@@ -16,22 +16,22 @@ tropin.tropin@gmail.com
 """
 
 def hr_max(ask_age):
-    out_hr_max = 205.8 - (0.685 * float(ask_age))
+    out_hr_max = 205.8 - (0.685 * int(ask_age))
     return out_hr_max
               
 def vo2_max(out_hr_max, ask_hr_rest):
-    out_vo2_max = 15 * (float(out_hr_max) / float(ask_hr_rest))
+    out_vo2_max = 15 * (int(out_hr_max) / int(ask_hr_rest))
     return out_vo2_max
               
 def bmr(ask_weight, ask_height, ask_age, ask_gender):
     if ask_gender.lower() in ['м', 'm']:
         my_gender = 'мужской'
-        out_bmr_male = 10 * float(ask_weight) + 6.25 * float(ask_height) - 5.0 * float(ask_age) + 5
+        out_bmr_male = 10 * float(ask_weight) + 6.25 * int(ask_height) - 5.0 * int(ask_age) + 5
         out_bmr = out_bmr_male
         return out_bmr, my_gender
     elif ask_gender.lower() in ['ж', 'f', 'w']:
         my_gender = 'женский'
-        out_bmr_woman = 10 * float(ask_weight) + 6.25 * float(ask_height) - 5.0 * float(ask_age) - 161
+        out_bmr_woman = 10 * float(ask_weight) + 6.25 * int(ask_height) - 5.0 * int(ask_age) - 161
         out_bmr = out_bmr_woman
         return out_bmr, my_gender
 
@@ -43,28 +43,28 @@ while True:
         break
 
 while True:
-    ask_age = input("Сколько вам лет? ")
+    ask_age = input("Сколько вам лет?\n")
     if ask_age.isnumeric() != True:
         continue
     else:
         break
 
 while True:
-    ask_height = input("Какого вы роста (в см)? ")
+    ask_height = input("Какого вы роста (в см)?\n")
     if ask_height.isnumeric() != True:
         continue
     else:
         break
 
 while True:
-    ask_weight = input("Сколько вы весите (в кг)? ")
+    ask_weight = input("Сколько вы весите (в кг)?\n")
     if ask_weight.isnumeric() != True:
         continue
     else:
         break
 
 while True:
-    ask_hr_rest = input("Каков ваш пульс в состоянии покоя (в ударах в минуту)? ")
+    ask_hr_rest = input("Каков ваш пульс в состоянии покоя (в ударах в минуту)?\n")
     if ask_hr_rest.isnumeric() != True:
         continue
     else:
@@ -74,6 +74,7 @@ my_hr_max = hr_max(ask_age)
 my_vo2_max = vo2_max(hr_max(ask_age), ask_hr_rest)
 my_bmr, my_gender = bmr(ask_weight, ask_height, ask_age, ask_gender)
 
+print()
 print(f"Ваш пол: {my_gender}")
 print(f"Ваш максимальный пульс: {my_hr_max:.0f} уд. в минуту.")
 print(f"Максимальный объём потребляемого кислорода составит:\n{my_vo2_max:.2f} мл на кг массы тела в минуту")
