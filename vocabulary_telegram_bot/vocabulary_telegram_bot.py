273524bcbd1d64a28cc6146178a51729fd9a61ce
Valery Tropin <tropin.tropin@gmail.com>
2022-09-28 14:33:28 +0300

    Fix vocabulary_telegram_bot/vocabulary_telegram_bot.py

--- vocabulary_telegram_bot/vocabulary_telegram_bot.py
+++ vocabulary_telegram_bot/vocabulary_telegram_bot.py
@@ -1,69 +1,63 @@
-'''
-A simple Telegram bot for Hexlet's practice.
-'''
+"""
+A simple Telegram bot for Hexlet"s practice.
+"""
 
+import json
 import random
 
-# '': '',
-en_ru_greetings = {
-    'здравствуйте': 'hello',
-    'привет': 'hi',
-    'добрый вечер': 'good evening',
-    'добрый день': 'good day',
-    'доброе утро': 'good morning',
-    'доброй ночи': 'good night',
-    'до свидания': 'good bye',
-    'пока': 'bye',
-    'до встречи': 'see you'
-}
 
-en_ru_hotel = {
-    'гостиница, отель': 'hotel',
-    'номер на одного': 'single room',
-    'номер на двоих': 'double room',
-    'Я бы хотел заказать номер': 'I want to order a room',
-    'У вас есть свободные номера?': 'Do you have a room?',
-    'регистрация (администратор)': 'reception',
-    'Сколько стоит номер в сутки на человека?': 'How does it cost a night per a man?',
-    'Я оплачу наличными': 'I pay in cash',
-    'Не работает свет': 'Something wrong with light'
-}
+def dict_open():
+    with open('cards.json', encoding='utf-8') as cards_file:
+        cards = json.load(cards_file)
+        return cards
 
-print('Tell me what theme do you want to repeat?\nGreetings - enter 1. Hotel - enter 2.')
-theme_choice = int(input('Enter your choice here: '))
-if theme_choice == 1:
-    words = en_ru_greetings
-elif theme_choice == 2:
-    words = en_ru_hotel
 
-level_choice = int(input('Choose the level of difficulty: enter 1 for easy and 2 for hard.'))
-if level_choice == 1:
-    level = 4
-elif level_choice == 2:
-    level = 9
+def theme_decision(cards):
+    print("Tell me what theme do you want to repeat?\nGreetings - enter 1. Hotel - enter 2.")
+    theme_choice = int(input("Enter your choice here: "))
+    if theme_choice == 1:
+        for words in cards["en_ru_greetings"]:
+            return words
+    elif theme_choice == 2:
+        for words in cards["en_ru_hotel"]:
+            return words
 
-counter = 5
-while counter:
-    answers = random.sample(list(words.keys()), level)
-    question = random.choice(answers)
 
-    print(f'Which word matches "{question}"?')
+def level_decision():
+    level_choice = int(input("Choose the level of difficulty: enter 1 for easy and 2 for hard."))
+    if level_choice == 1:
+        level = 4
+    elif level_choice == 2:
+        level = 9
+    return level
 
-    correct_answer = 0
-    for i, answer in enumerate(answers, start=1):
-        if answer == question:
-            correct_answer = i
 
-        print(i, words[answer])
+def game(words, level):
+    counter = 5
+    while counter:
+        answers = random.sample(list(words.keys()), level)
+        question = random.choice(answers)
 
-    user_answer = int(input('Enter the correct number: '))
+        print(f"Which word matches '{question}'?")
 
-    if user_answer == correct_answer:
-        print('Correct!')
-        counter -= 1
-    else:
-        print(f'You failed! Correct answer was "{words[question]}"')
-        break
+        correct_answer = 0
+        for i, answer in enumerate(answers, start=1):
+            if answer == question:
+                correct_answer = i
 
-if not counter:
-    print('Congratulations! You did it!')
+            print(i, words[answer])
+
+        user_answer = int(input("Enter the correct number: "))
+
+        if user_answer == correct_answer:
+            print("Correct!")
+            counter -= 1
+        else:
+            print(f"You failed! Correct answer was '{words[question]}'")
+            break
+
+    if not counter:
+        print("Congratulations! You did it!")
+
+
+game(theme_decision(dict_open()), level_decision())
