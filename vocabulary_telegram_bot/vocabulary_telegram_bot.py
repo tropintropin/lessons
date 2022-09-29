00ff21a472180bd86916caa51b36998708af39cf
Valery Tropin <tropin.tropin@gmail.com>
2022-09-29 13:56:06 +0300

    Fix vocabulary_telegram_bot/vocabulary_telegram_bot.py
    Add input validation.

--- vocabulary_telegram_bot/vocabulary_telegram_bot.py
+++ vocabulary_telegram_bot/vocabulary_telegram_bot.py
@@ -5,9 +5,22 @@ A simple Telegram bot for Hexlet's practice.
 import json
 import random
 
+# from telegram.ext import Updater, CommandHandler, MessageHandler
+# from telegram.ext import Filters
+# from telegram import ReplyKeyboardMarkup
+
+
+# updater = Updater(token='TOKEN', use_context=True)
+
 
 def translation_direction():
-    direction = int(input("Choose the direction of translation.\nEnter 1 for Russian to English and 2 for English to Russian: "))
+    while True:
+        direction = int(input("Choose the direction of translation.\nEnter 1 for Russian to English and 2 for English to Russian: "))
+        if not direction.isnumeric() and (direction != 1 or direction != 2):
+            print('Please enter 1 or 2: ')
+            continue
+        else:
+            break
     return direction
 
 
@@ -19,7 +32,14 @@ def dict_open():
 
 def theme_decision(cards, direction):
     print("Tell me what theme do you want to repeat?\nGreetings - enter 1. Hotel - enter 2.")
-    theme_choice = int(input("Enter your choice here: "))
+    while True:
+        theme_choice = int(input("Enter your choice here: "))
+        if not theme_choice.isnumeric() and (theme_choice != 1 or theme_choice != 2):
+            print('Please enter 1 or 2: ')
+            continue
+        else:
+            break
+
     if theme_choice == 1:
         for words in cards["en_ru_greetings"]:
             # обратный словарь сюда
@@ -35,7 +55,14 @@ def theme_decision(cards, direction):
 
 
 def level_decision():
-    level_choice = int(input("Choose the level of difficulty.\nEnter 1 for easy and 2 for hard: "))
+    while True:
+        level_choice = int(input("Choose the level of difficulty.\nEnter 1 for easy and 2 for hard: "))
+        if not level_choice.isnumeric() and (level_choice != 1 or level_choice != 2):
+            print('Please enter 1 or 2: ')
+            continue
+        else:
+            break
+
     if level_choice == 1:
         level = 4
     elif level_choice == 2:
@@ -58,7 +85,13 @@ def game(words, level):
 
             print(i, words[answer])
 
-        user_answer = int(input("Enter the correct number: "))
+        while True:
+            user_answer = int(input("Enter the correct number: "))
+            if not user_answer.isnumeric():
+                print('Please enter a number: ')
+                continue
+            else:
+                break
 
         if user_answer == correct_answer:
             print("Correct!")
