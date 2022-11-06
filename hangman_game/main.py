60b00f99e0860ac3161f743cf0555a392a5eb5d6
Valery Tropin <tropin.tropin@gmail.com>
2022-11-06 16:09:36 +0300

    Fix hangman_game/main.py
    Add get_letter() function.

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -3,7 +3,22 @@ import random
 from stages import display_hangman
 
 
+def get_letter() -> str:
+    while True:
+        letter = input('Введите букву: ').upper()
+        if 1039 < ord(letter) < 1072:  # Only cirillic upper letters
+            return letter
+
+
 def get_word() -> str:
     with open('words.json', 'r', encoding='utf-8') as f:
         word_list = json.load(f)
     return random.choice(word_list).upper()
+
+
+# def play(word: str):
+#     word_completion = '_' * len(word)
+#     guessed = False
+#     guessed_letters = []
+#     guessed_words = []
+#     tries = 6
