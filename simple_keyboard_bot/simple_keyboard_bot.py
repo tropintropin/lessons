b1a93b9f70aea0b5a266449bfad99e719763da62
Valery Tropin <tropin.tropin@gmail.com>
2022-12-09 02:04:05 +0300

    Move simple_quiz_bot/simple_keyboard_bot.py to simple_keyboard_bot/simple_keyboard_bot.py

--- simple_keyboard_bot/simple_keyboard_bot.py
+++ simple_keyboard_bot/simple_keyboard_bot.py
@@ -19,7 +19,8 @@ keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
 keyboard.add(KeyboardButton(text='Отправить телефон', request_contact=True))
 keyboard.add(KeyboardButton(text='Отправить геолокацию', request_location=True))
-keyboard.add(KeyboardButton(text='Создать опрос/викторину', request_poll=KeyboardButtonPollType()))
+keyboard.add(KeyboardButton(text='Создать викторину', request_poll=KeyboardButtonPollType(type=types.PollType.QUIZ)))
+keyboard.add(KeyboardButton(text='Создать опрос', request_poll=KeyboardButtonPollType(type=types.PollType.REGULAR)))
 
 
 async def process_start_command(message: types.Message):
