255ebadde18169374967b381e62ca89b7bd9bbd3
Valery Tropin <tropin.tropin@gmail.com>
2022-12-09 01:57:08 +0300

    Add simple_quiz_bot/simple_keyboard_bot.py
    Create a simple Telegram bot with a sample asking keyboards (phone,
    location etc.)

---
+++ simple_quiz_bot/simple_keyboard_bot.py
@@ -0,0 +1,32 @@
+# A simple Telegram bot with keyboards
+# https://stepik.org/lesson/759405/step/4?unit=761421
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonPollType
+
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env('BOT_TOKEN')
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+keyboard.add(KeyboardButton(text='Отправить телефон', request_contact=True))
+keyboard.add(KeyboardButton(text='Отправить геолокацию', request_location=True))
+keyboard.add(KeyboardButton(text='Создать опрос/викторину', request_poll=KeyboardButtonPollType()))
+
+
+async def process_start_command(message: types.Message):
+    await message.answer('Экспериментируем со специальными кнопками', reply_markup=keyboard)
+
+
+dp.register_message_handler(process_start_command, commands='help')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
