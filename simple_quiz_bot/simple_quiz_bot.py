3cbef5fc2a0810aa81502582db07eebbd8113083
Valery Tropin <tropin.tropin@gmail.com>
2022-12-09 02:05:06 +0300

    Fix simple_quiz_bot/simple_quiz_bot.py

--- simple_quiz_bot/simple_quiz_bot.py
+++ simple_quiz_bot/simple_quiz_bot.py
@@ -1,5 +1,8 @@
+# A simple Telegram bot with keyboards
+# https://stepik.org/lesson/759405/step/2?unit=761421
+
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, KeyboardButtonPollType
 
 from environs import Env
 
@@ -11,3 +14,36 @@ BOT_TOKEN = env('BOT_TOKEN')
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+button1 = KeyboardButton('Собак 🦮')
+button2 = KeyboardButton('Огурцов 🥒')
+
+
+keyboard.add(button1, button2)
+
+async def process_start_command(message: types.Message):
+    await message.answer('Чего кошки боятся больше?', reply_markup=keyboard)
+
+
+async def process_dog_answer(message: types.Message):
+    await message.answer(
+        'Да, несомненно, кошки боятся собак. Но вы видели как они боятся огурцов?',
+        reply_markup=ReplyKeyboardRemove()
+        )
+
+
+async def process_cucumber_answer(message: types.Message):
+    await message.answer(
+        'Да, иногда кажется, что огурцов кошки боятся больше',
+        reply_markup=ReplyKeyboardRemove()
+        )
+
+
+dp.register_message_handler(process_start_command, commands='start')
+dp.register_message_handler(process_dog_answer, text='Собак 🦮')
+dp.register_message_handler(process_cucumber_answer, text='Огурцов 🥒')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
