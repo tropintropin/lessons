7aa5ea0de205424d10c638994097bee31d28944d
Valery Tropin <tropin.tropin@gmail.com>
2022-12-09 19:40:42 +0300

    Add rock_paper_scissors_bot/keyboards/menu_button.py

---
+++ rock_paper_scissors_bot/keyboards/menu_button.py
@@ -0,0 +1,11 @@
+# https://stepik.org/lesson/792844/step/2?unit=795518
+
+from aiogram import Dispatcher, types
+
+
+async def set_main_menu(dp: Dispatcher):
+    main_menu_commands = [
+        types.BotCommand(command='/start', description='Запустить бота'),
+        types.BotCommand(command='/help', description='Справка по работе бота')
+    ]
+    await dp.bot.set_my_commands(main_menu_commands)
