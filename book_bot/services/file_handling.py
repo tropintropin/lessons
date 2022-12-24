c2e3a98745b909fa124f39a515a7dcd1a5bd6a06
Valery Tropin <tropin.tropin@gmail.com>
2022-12-24 17:46:55 +0300

    Fix book_bot/services/file_handling.py

---
+++ book_bot/services/file_handling.py
@@ -0,0 +1,20 @@
+# https://stepik.org/lesson/759408/step/7?unit=761424
+
+BOOK_PATH = 'book/book.txt'
+PAGE_SIZE = 1050
+
+book: dict[int, str] = {}
+
+
+# Функция, возвращающая строку с текстом страницы и ее размер
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:  # pyright: ignore
+    pass
+
+
+# Функция, формирующая словарь книги
+def prepare_book(path: str) -> None:
+    pass
+
+
+# Вызов функции prepare_book для подготовки книги из текстового файла
+prepare_book(BOOK_PATH)
