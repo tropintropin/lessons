9fcd487ce5ad360b280d69aff8b8643e59f7eaea
Valery Tropin <tropin.tropin@gmail.com>
2023-06-11 11:11:21 +0300

    Fix weather_forecast_yandex_practicum/main.py
    Here is the first iteretion of the script with the OpenWeather API ID
    from Yandex Practicum.

---
+++ weather_forecast_yandex_practicum/main.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+
+city = "Санкт-Петербург"
+
+server = "https://api.openweathermap.org/data/2.5/weather"
+
+# Here are the Yandex Practicum's OpenWeather API ID!
+parameters = "&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347"
+
+url = f'{server}?q={city}{parameters}'
+
+weather_data = requests.get(url).json()
+
+weather_data_structure = json.dumps(weather_data, indent=2)
+
+# print(weather_data_structure)
+
+temperature = round(weather_data['main']['temp'])
+temperature_feels = round(weather_data['main']['feels_like'])
+
+print(f'Сейчас в городе {city} {temperature}°C')
+print(f'Ощущается как {temperature_feels}°C')
+
